ce68a7bf238d781ce2b7379707fde73a003660bf
Yochen-Chang <hi.changyc@gmail.com>
2023-06-23 14:58:13 +0800

    [DEV] 新增防止 Spam 機制

--- src/components/contact.js
+++ src/components/contact.js
@@ -9,8 +9,9 @@ const Contact = () => {
                     <h2>Contact</h2>
                 </header>
 
-                <form name="Contact Form" method="POST" data-netlify="true">
+                <form name="Contact Form" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
                     <input type="hidden" name="form-name" value="Contact Form" />
+                    <input class="d-none" name="bot-field" />
                     <div class="row">
                         <div class="col-6 col-12-mobile">
                             <input type="text" class="form-control" id="name" name="name" placeholder="Name" />
