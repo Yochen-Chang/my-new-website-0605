3629a1686b30705e2fc87f336814c1eef263cc97
Yochen-Chang <hi.changyc@gmail.com>
2023-06-23 14:55:17 +0800

    [DEV] 新增傳送成功訊息（移除）

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,10 +1,6 @@
 import React, { useState } from 'react';
 
 const Contact = () => {
-    const [isFormSubmitted, setIsFormSubmitted] = useState(false);
-    const handleSubmit = (event) => {
-        setIsFormSubmitted(true);
-    };
     return (
         <section id="contact">
             <div className="container">
@@ -13,25 +9,21 @@ const Contact = () => {
                     <h2>Contact</h2>
                 </header>
 
-                {isFormSubmitted ? (
-                    <p>已成功傳送！感謝您的訊息。</p>
-                ) : (
-                    <form name="Contact Form" method="POST" data-netlify="true" action="" onSubmit={handleSubmit}>
-                        <input type="hidden" name="form-name" value="Contact Form" />
-                        <div class="row">
-                            <div class="col-6 col-12-mobile">
-                                <input type="text" class="form-control" id="name" name="name" placeholder="Name" />
-                            </div>
-                            <div class="col-6 col-12-mobile">
-                                <input type="email" class="form-control" id="inputEmail" name="email"  placeholder="Email" />
-                            </div>
-                            <div className="col-12">
-                                <textarea name="message" placeholder="Message"></textarea>
-                            </div>
+                <form name="Contact Form" method="POST" data-netlify="true">
+                    <input type="hidden" name="form-name" value="Contact Form" />
+                    <div class="row">
+                        <div class="col-6 col-12-mobile">
+                            <input type="text" class="form-control" id="name" name="name" placeholder="Name" />
                         </div>
-                        <button type="submit">Send</button>
-                    </form>
-                )}
+                        <div class="col-6 col-12-mobile">
+                            <input type="email" class="form-control" id="inputEmail" name="email"  placeholder="Email" />
+                        </div>
+                        <div className="col-12">
+                            <textarea name="message" placeholder="Message"></textarea>
+                        </div>
+                    </div>
+                    <button type="submit">Send</button>
+                </form>
             </div>
         </section>
     )
