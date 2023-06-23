29fe236d72e3f8f00760891807ce32c6b73e377f
Yochen-Chang <hi.changyc@gmail.com>
2023-06-23 14:23:55 +0800

    [DEV] 新增傳送成功訊息

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 
 const Contact = () => {
+    const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+    const handleSubmit = (event) => {
+        setIsFormSubmitted(true);
+    };
     return (
         <section id="contact">
             <div className="container">
@@ -9,21 +13,25 @@ const Contact = () => {
                     <h2>Contact</h2>
                 </header>
 
-                <form name="Contact Form" method="POST" data-netlify="true">
-                    <input type="hidden" name="form-name" value="Contact Form" />
-                    <div class="form-row">
-                        <div class="form-group col-md-6 mt-3">
-                            <label for="name">Your Name</label>
-                            <input type="text" class="form-control" id="name" name="name" />
+                {isFormSubmitted ? (
+                    <p>已成功傳送！感謝您的訊息。</p>
+                ) : (
+                    <form name="Contact Form" method="POST" data-netlify="true" onSubmit={handleSubmit}>
+                        <input type="hidden" name="form-name" value="Contact Form" />
+                        <div class="row">
+                            <div class="col-6 col-12-mobile">
+                                <input type="text" class="form-control" id="name" name="name" placeholder="Name" />
+                            </div>
+                            <div class="col-6 col-12-mobile">
+                                <input type="email" class="form-control" id="inputEmail" name="email"  placeholder="Email" />
+                            </div>
+                            <div className="col-12">
+                                <textarea name="message" placeholder="Message"></textarea>
+                            </div>
                         </div>
-                        <div class="form-group col-md-6 mt-3">
-                            <label for="inputEmail">Email</label>
-                            <input type="email" class="form-control" id="inputEmail" name="email" />
-                        </div>
-                    </div>
-                    <button type="submit">Send</button>
-                </form>
-
+                        <button type="submit">Send</button>
+                    </form>
+                )}
             </div>
         </section>
     )
