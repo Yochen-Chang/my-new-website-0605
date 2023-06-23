837c3f6ad7254e6fc6905ba848375773a2630b01
Yochen-Chang <hi.changyc@gmail.com>
2023-06-23 10:48:43 +0800

    [DEV] 新增 Contact 表單功能

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,6 +1,16 @@
-import * as React from 'react'
+import React, { useState } from 'react';
 
 const Contact = () => {
+    const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // 在這裡處理表單提交的邏輯，例如發送郵件或儲存資料的後端處理
+
+        // 設定表單提交成功的狀態
+        setIsFormSubmitted(true);
+    };
+
     return (
         <section id="contact">
             <div className="container">
@@ -9,18 +19,22 @@ const Contact = () => {
                     <h2>Contact</h2>
                 </header>
 
-                <form method="post" action="#">
-                    <div className="row">
-                        <div className="col-6 col-12-mobile"><input type="text" name="name" placeholder="Name" /></div>
-                        <div className="col-6 col-12-mobile"><input type="text" name="email" placeholder="Email" /></div>
-                        <div className="col-12">
-                            <textarea name="message" placeholder="Message"></textarea>
+                {isFormSubmitted ? (
+                    <p>表單已成功提交！感謝您的訊息。</p>
+                ) : (
+                    <form onSubmit={handleSubmit}>
+                        <div className="row">
+                            <div className="col-6 col-12-mobile"><input type="text" name="name" placeholder="Name" /></div>
+                            <div className="col-6 col-12-mobile"><input type="text" name="email" placeholder="Email" /></div>
+                            <div className="col-12">
+                                <textarea name="message" placeholder="Message"></textarea>
+                            </div>
+                            <div className="col-12">
+                                <input type="submit" value="Send Message" />
+                            </div>
                         </div>
-                        <div className="col-12">
-                            <input type="submit" value="Send Message" />
-                        </div>
-                    </div>
-                </form>
+                    </form>
+                )}
 
             </div>
         </section>
