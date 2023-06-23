1bca99d0c2d0ece5380a28ed11d1de0f80fef4da
Yochen-Chang <hi.changyc@gmail.com>
2023-06-23 13:09:11 +0800

    [DEV] 新增訊息傳送成功頁面

---
+++ src/pages/success.js
@@ -0,0 +1,13 @@
+//  src/pages/about.js
+import React from "react"
+
+const SuccessPage = () => {
+  return (
+    <div>
+      <h1>訊息已傳送</h1>
+      <p>已收到您的訊息，會盡快回覆您！</p>
+    </div>
+  )
+}
+
+export default SuccessPage
